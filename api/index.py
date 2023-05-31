14bbc7afef45f841736e7c53c1124d66f4f30d61
Filsommer <filsommer@hotmail.com>
2023-05-31 17:51:52 +0200

    adds prisma

--- api/index.py
+++ api/index.py
@@ -66,6 +66,7 @@ async def create_item(item: Items):
 async def get_items():
     # write your queries here
     #items: List[Items] = await prisma.items.find_many(where={"name": {"contains": "it"}})
+    
     return []
 
 @app.get("/api/python", response_model=str)
