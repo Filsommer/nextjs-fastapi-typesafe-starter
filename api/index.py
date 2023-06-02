28e71621449829a54424a7807bb742e6dc3f0731
Filsommer <filsommer@hotmail.com>
2023-06-02 11:56:17 +0200

    fixes prisma

--- api/index.py
+++ api/index.py
@@ -70,13 +70,14 @@ async def shutdown():
 @app.middleware("https")
 async def middleware_ep(request: Request, call_next):
     if not is_production:
-        print("ho-hey")
         response = await call_next(request)
     else:  # endpoints are lambdas on vercel, so we need to instantiate prisma on every request :(
-        await prisma.connect()
+        try:
+            await prisma.connect()
+        except PermissionError:
+            pass
         response = await call_next(request)
         await prisma.disconnect()
-        response = await call_next(request)
     return response
 
 
